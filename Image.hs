f6ed5a7b6863d5232655d3cbffff44a8eb9c28bd
Matthias Treydte <waldheinz@gmail.com>
2010-04-22 22:11:24 +0200

    Some initial code for the MArray-backed image.

--- Image.hs
+++ Image.hs
@@ -1,12 +1,31 @@
 
-module Image(makePgm) where
+module Image(makePgm, makeImage) where
+
+import Control.Monad.ST
+import Data.Array.ST
 
 import Color
+
+type WeightedSpectrum = (Float, Spectrum)
 
 data Image = Image {
    imageWidth :: Int,
    imageHeight :: Int
-   }
+ --  imagePixels :: (STArray s Int WeightedSpectrum)
+   } deriving Show
+
+makeImage w h = do
+   arr <- newArray (0, pxCount) v
+   return (w, h, arr) where
+      pxCount = w * h :: Int
+      v = (0.0, black) :: WeightedSpectrum
+         
+
+-- | creates an all-black image of the specified width and height
+--makeImage :: Int -> Int -> Image
+--makeImage w h = Image w h pixels where
+--   pxCount = w * h
+--   pixels = array (0, pxCount) $ [(i, (0, black)) | i <- [0..pxCount]]
    
 clamp :: Float -> Int
 clamp v = round ( (min 1 (max 0 v)) * 255 )
