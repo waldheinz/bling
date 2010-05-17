ede8275dc39e62d153e0abaa1ee8047e7117db46
Matthias Treydte <waldheinz@gmail.com>
2010-05-17 20:37:30 +0200

    Added ray triangle intersection test.

--- TriangleMesh.hs
+++ TriangleMesh.hs
@@ -1,9 +1,34 @@
 
 module TriangleMesh where
 
+import Geometry
 import Math
 
-newtype Vertex = Vertex Point
+data Vertex = Vertex {
+   vertexPos :: Point
+   }
 
-newtype Triangle = Triangle (Int, Int, Int)
+data Triangle = Triangle Vertex Vertex Vertex
 
+instance Intersectable Triangle where
+   intersect (Triangle v1 v2 v3) r@(Ray ro rd tmin tmax)
+      | divisor == 0 = Nothing
+      | b1 < 0 || b1 > 1 = Nothing
+      | b2 < 0 || b1 + b2 > 1 = Nothing
+      | t < tmin || t > tmax = Nothing
+      | otherwise = Just (t, DifferentialGeometry (positionAt r t) (normalize $ cross e1 e2))
+      where
+            t = (dot e2 s2) * invDiv
+            b2 = (dot e2 s2) * invDiv -- second barycentric
+            s2 = cross d e1
+            b1 = (dot d s1) * invDiv -- first barycentric
+            d = sub ro p1
+            invDiv = 1 / divisor
+            divisor = dot s1 e1
+            s1 = cross rd e2
+            e1 = sub p2 p1
+            e2 = sub p3 p1
+            p1 = vertexPos v1
+            p2 = vertexPos v2
+            p3 = vertexPos v3
+            
