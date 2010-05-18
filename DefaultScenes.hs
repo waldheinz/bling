0ff81d3d0f19c25faa26c03a5ba801170ed51efd
Matthias Treydte <waldheinz@gmail.com>
2010-05-18 13:01:14 +0200

    Fixed ray-triangle intersection.

--- DefaultScenes.hs
+++ DefaultScenes.hs
@@ -30,7 +30,7 @@ plTest e kd  = plasticMaterial
 
 boxScene :: Float -> Scene
 boxScene aspect = trace (show faces) $ mkScene [SoftBox $ fromRGB (0.95, 0.95, 0.95)] 
-   [box ]-- , mkPrim (Plane 1.5 ( 0,  1,  0)) (measuredMaterial BrushedMetal)]
+   [box , mkPrim (Plane 1.5 ( 0,  1,  0)) (measuredMaterial BrushedMetal)]
    (pinHoleCamera (View (7, 7, -7) (0,0,0) (0, 1, 0) 1.8 aspect))
    where
          box = Group $ map (\g -> mkPrim g (measuredMaterial BluePaint)) faces
