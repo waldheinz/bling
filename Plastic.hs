72cd874b1746b368d099bdf68c76bfa8d15d83cf
Matthias Treydte <waldheinz@gmail.com>
2011-02-20 23:07:17 +0100

    Fixed hlint warnings in Plastic module.

--- Plastic.hs
+++ Plastic.hs
@@ -10,7 +10,7 @@ import Transport
    
 plasticMaterial :: SpectrumTexture -> SpectrumTexture -> Float -> Material
 plasticMaterial kd ks rough dg = mkBsdf [diff, spec] sc where
-   diff = MkAnyBxdf $ Lambertian $ rd
+   diff = MkAnyBxdf $ Lambertian rd
    spec = MkAnyBxdf $ Microfacet (Blinn (1 / rough)) (frDiel 1.0 1.5) rs
    rd = kd dg
    rs = ks dg
