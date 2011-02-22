76bb135a49ae2f32d2b2f637c60343538b404f9c
Matthias Treydte <waldheinz@gmail.com>
2011-02-22 21:23:26 +0100

    Fixed formatting in Random module.

--- src/Random.hs
+++ src/Random.hs
@@ -8,17 +8,17 @@ import System.Random.MWC
 
 type Rand2D = (Float, Float)
 
--- | Marks a computation that requires random values                                                                                         
+-- | Marks a computation that requires random values
 newtype Rand a = Rand {
    unR :: (forall s. Gen s -> ST s a)
    }
 
--- | For allowing the Monadic syntax when using @Rand@                                                                                       
+-- | For allowing the Monadic syntax when using @Rand@
 instance Monad Rand where
     return k = Rand (\ _ -> return k)
     Rand c1 >>= fc2 = Rand (\ g -> c1 g >>= \a -> unR (fc2 a) g)
 
--- | Lets a computation run in the Rand Monad                                                                                                
+-- | Lets a computation run in the Rand Monad
 runRand :: Int -> Rand a -> a
 runRand seed (Rand c) = runST (do gen <- initialize $ singleton $ fromIntegral seed
                                   c gen)
@@ -30,7 +30,7 @@ runRandST gen (Rand c) = do
 --   gen <- initialize $ singleton $ fromIntegral seed
    c gen
 
--- | Provides a random @Float@ in @[0..1)@                                                                                                   
+-- | Provides a random @Float@ in @[0..1)@
 rnd :: Rand Float
 rnd = do
    u <- Rand uniform
@@ -41,7 +41,7 @@ rndRI (lo, hi) = do
    t <- rnd
    return $ lo + round (t * fromIntegral (hi - lo))
 
--- | Provides a random @Float@ in the specified range (left inclusive, right exclusive)                                                      
+-- | Provides a random @Float@ in the specified range (left inclusive, right exclusive)
 rndR :: (Float, Float) -> Rand Float
 rndR (lo, hi) = do
    t <- rnd
