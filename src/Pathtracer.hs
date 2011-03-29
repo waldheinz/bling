b859b485389c998badbeb0feb8708b43104a0e09
Matthias Treydte <waldheinz@gmail.com>
2011-03-29 19:02:03 +0200

    Fixed hlint warnings in Scene module.

--- src/Pathtracer.hs
+++ src/Pathtracer.hs
@@ -3,7 +3,6 @@ module Pathtracer (pathTracer) where
 
 import Data.BitSet
 import Data.Array.Unboxed
-import Debug.Trace
 
 import Light
 import Math
@@ -20,7 +19,7 @@ directLight :: Scene -> Ray -> Spectrum
 directLight s ray = foldl (+) black (map (`le` ray) (elems $ sceneLights s))
 
 nextVertex :: Scene -> Int -> Bool -> Ray -> Maybe Intersection -> Spectrum -> Spectrum -> Rand WeightedSpectrum
--- nextVertex _ 10 _ _ _ _ l = return $! (1.0, seq l l) -- hard bound
+nextVertex _ 1 _ _ _ _ l = return $! (1.0, seq l l) -- hard bound
 
 nextVertex s _ True ray Nothing throughput l = -- nothing hit, specular bounce
    return $! (1.0, l + throughput * directLight s ray)
@@ -33,16 +32,16 @@ nextVertex scene depth specBounce (Ray _ rd _ _) (Just int) throughput l
    | otherwise = do
       bsdfDirU <- rnd2D
       bsdfCompU <- rnd
-      let (BsdfSample smpType pdf f wi) = sampleBsdf bsdf wo bsdfCompU bsdfDirU
+      let (BsdfSample smpType spdf f wi) = sampleBsdf bsdf wo bsdfCompU bsdfDirU
       ulNum <- rnd
       lHere <- sampleOneLight scene p n wo bsdf ulNum
       let outRay = (Ray p wi epsilon infinity)
-      let throughput' = throughput * sScale f (absDot wi n / pdf)
+      let throughput' = throughput * sScale f (absDot wi n / spdf)
       let l' = l + (throughput * (lHere + intl))
       let spec' = Specular `member` smpType
       
       x <- rnd
-      if x > pCont || (pdf == 0.0)
+      if x > pCont || (spdf == 0.0)
          then return $! (1.0, l')
          else nextVertex scene (depth + 1) spec' outRay (intersect (scenePrim scene) outRay) throughput' l'
          
--- src/Scene.hs
+++ src/Scene.hs
@@ -5,7 +5,7 @@ module Scene (
 
 import Control.Monad
 import Data.Array.Unboxed
-import Data.Maybe (isJust, fromJust, catMaybes)
+import Data.Maybe (isJust, fromJust, catMaybes, mapMaybe)
 import Text.PrettyPrint
 
 import Bvh
@@ -27,7 +27,7 @@ data Scene = Scene {
 ppScene :: Scene -> Doc
 ppScene (Scene p ls _) = vcat [
    text "bounds" <+> text (show (worldBounds p)),
-   text "number of lights" <+> (int (rangeSize (bounds ls))),
+   text "number of lights" <+> int (rangeSize (bounds ls)),
    text "BVH stats" $$ nest 3 (ppBvh p)
    ]
    
@@ -35,7 +35,7 @@ mkScene :: (Primitive a) => [Light] -> [a] -> Camera -> Scene
 mkScene l prims cam = Scene (mkBvh ps) la cam where
    la = listArray (0, length lights - 1) lights
    lights = l ++ gl
-   gl = catMaybes $ map light ps -- collect the geometric lights
+   gl = mapMaybe light ps -- collect the geometric lights
    ps = concatMap flatten prims
    
 occluded :: Scene -> Ray -> Bool
@@ -51,7 +51,7 @@ evalSample :: Scene -> LightSample -> Vector -> Bsdf -> Point -> Normal -> Spect
 evalSample scene s wo bsdf _ n
    | isBlack li || isBlack f = black
    | occluded scene (testRay s) = black
-   | otherwise = sScale (f * li) $ (absDot wi n) / lPdf
+   | otherwise = sScale (f * li) $ absDot wi n / lPdf
    where
          lPdf = lightSamplePdf s
          li = de s
@@ -60,24 +60,24 @@ evalSample scene s wo bsdf _ n
    
 sampleLightMis :: Scene -> LightSample -> Bsdf -> Vector -> Normal -> Spectrum
 sampleLightMis scene (LightSample li wi ray p deltaLight) bsdf wo n
-   | (p == infinity) || (isBlack li) || (isBlack f) || (occluded scene ray) = black
-   | deltaLight = sScale (f * li) ((absDot wi n) / p)
-   | otherwise = sScale (f * li) ((absDot wi n) * weight / p)
+   | p == infinity || isBlack li || isBlack f || occluded scene ray = black
+   | deltaLight = sScale (f * li) (absDot wi n / p)
+   | otherwise = sScale (f * li) (absDot wi n * weight / p)
    where
          f = evalBsdf bsdf wo wi
          weight = powerHeuristic (1, p) (1, bsdfPdf bsdf wo wi)
 
 sampleBsdfMis :: Scene -> Light -> BsdfSample -> Normal -> Point -> Spectrum
 sampleBsdfMis (Scene sp _ _) l (BsdfSample _ bPdf f wi) n p
-   | (isBlack f) || (bPdf == 0) = black
+   | isBlack f || bPdf == 0 = black
    | isJust lint = scale $ intLe (fromJust lint) (neg wi) -- TODO: need to check if the "right" light was hit
    | otherwise = scale (le l ray)
    where
          lPdf = pdf l p wi
          weight = powerHeuristic (1, bPdf) (1, lPdf)
-         scale = (\li -> sScale (f * li) ((absDot wi n) * weight / bPdf))
+         scale li = sScale (f * li) (absDot wi n * weight / bPdf)
          ray = Ray p wi epsilon infinity
-         lint = intersect sp ray
+         lint = sp `intersect` ray
          
 -- | samples all lights by sampling individual lights and summing up the results
 sampleAllLights :: Scene -> Point -> Normal -> Vector -> Bsdf -> Rand Spectrum
@@ -88,18 +88,20 @@ estimateDirect s l p n wo bsdf =
    let lSmp = sample l p n
        bSmp = sampleBsdf bsdf wo
    in do
-   uL <- rnd2D
-   uBC <- rnd
-   uBD <- rnd2D
-   return $ (sampleLightMis s (lSmp uL) bsdf wo n) + (sampleBsdfMis s l (bSmp uBC uBD) n p)
+      uL <- rnd2D
+      uBC <- rnd
+      uBD <- rnd2D
+      let ls = sampleLightMis s (lSmp uL) bsdf wo n
+      let bs = sampleBsdfMis s l (bSmp uBC uBD) n p
+      return (ls + bs)
    
 -- | samples one randomly chosen light source
 sampleOneLight :: Scene -> Point -> Normal -> Vector -> Bsdf -> Float -> Rand Spectrum
 sampleOneLight scene@(Scene _ lights _) p n wo bsdf ulNum
-   | lightCount == 0 = return $ black
+   | lightCount == 0 = return black
    | lightCount == 1 = estimateDirect scene (lights ! 0)  p n wo bsdf
-   | otherwise = (liftM scale) (estimateDirect scene (lights ! lightNum) p n wo bsdf)
+   | otherwise = liftM scale (estimateDirect scene (lights ! lightNum) p n wo bsdf)
       where
             lightCount = snd (bounds lights) + 1
-            lightNum = floor $ ulNum * (fromIntegral lightCount)
-            scale = (\y -> sScale y (fromIntegral lightCount))
+            lightNum = floor $ ulNum * fromIntegral lightCount
+            scale y = sScale y (fromIntegral lightCount)
