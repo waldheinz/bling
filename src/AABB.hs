3d9b0b923b1797f265d28dd02e6eb25e3abc9e94
Matthias Treydte <waldheinz@gmail.com>
2011-03-29 21:27:59 +0200

    Removed Storable cruft from AABB module.

--- src/AABB.hs
+++ src/AABB.hs
@@ -4,9 +4,6 @@ module AABB (
    emptyAABB, extendAABB, extendAABBP, maximumExtent, centroid, intersectAABB
 ) where
 
-import Foreign.Storable
-import Foreign
-
 import Math
 
 -- | an axis-aligned bounding box
@@ -18,30 +15,6 @@ data AABB = AABB {
 instance Show AABB where
    show b = "AABB [min=" ++ show (aabbMin b) ++ ", max="
       ++ show (aabbMax b) ++ "]"
-
-instance Storable AABB where
-   sizeOf _ = sizeOf (undefined :: Flt) * 6
-   alignment _ = alignment (undefined :: Flt)
-   
---    peek p = do
---       x1 <- peekElemOff q 0 :: IO Flt
---       y1 <- peekElemOff q 1 :: IO Flt
---       z1 <- peekElemOff q 2 :: IO Flt
---       x2 <- peekElemOff q 3 :: IO Flt
---       y2 <- peekElemOff q 4 :: IO Flt
---       z2 <- peekElemOff q 5 :: IO Flt
---       return (AABB (x1, y1, z1) (x2, y2, z2)) where
--- 	 q = castPtr p
-      
---    poke p (AABB (x1, y1, z1) (x2, y2, z2)) = do
---       pokeElemOff q 0 x1
---       pokeElemOff q 0 y1
---       pokeElemOff q 0 z1
---       pokeElemOff q 0 x2
---       pokeElemOff q 0 y2
---       pokeElemOff q 0 z2
---       where
--- 	 q = castPtr p
 
 emptyAABB :: AABB
 emptyAABB = AABB
