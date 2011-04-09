dff5f80f04ccc68b5ea8058f6780d270b81d3898
Matthias Treydte <waldheinz@gmail.com>
2011-04-09 15:56:15 +0200

    Improved shape syntax, moved parser to separate module.

--- src/Graphics/Bling/IO/RenderJob.hs
+++ src/Graphics/Bling/IO/RenderJob.hs
@@ -10,15 +10,13 @@ import Graphics.Bling.Filter
 import Graphics.Bling.Light
 import Graphics.Bling.Math
 import Graphics.Bling.Pathtracer
-import Graphics.Bling.Primitive
 import Graphics.Bling.Scene
-import Graphics.Bling.Shape
 import Graphics.Bling.Transform
 import Graphics.Bling.IO.MaterialParser
 import Graphics.Bling.IO.ParserCore
+import Graphics.Bling.IO.ShapeParser
 import Graphics.Bling.IO.TransformParser
 
-import qualified Data.Vector as V
 import Text.ParserCombinators.Parsec
 import qualified Text.PrettyPrint as PP
 
@@ -66,7 +64,7 @@ jobParser = do
 
 object :: JobParser ()
 object = 
-       do pShape
+       do try pShape
       <|> pCamera
       <|> pFilter
       <|> try pSize
@@ -76,51 +74,6 @@ object =
       <|> pMaterial
       <|> pTransform
       <|> ws
-
---
--- parsing shapes
---
-
-pShape :: JobParser ()
-pShape = do
-   try (string "beginShape") >> ws
-   _ <- string "type" >> ws
-   t <- many alphaNum
-   ws
-   sh <- case t of
-      "sphere" -> do
-         r <- namedFloat "radius"
-         return [mkSphere r]
-      "mesh" -> pMesh
-      
-      _ -> fail ("unknown shape type \"" ++ t ++ "\"")
-   _ <- ws >> string "endShape"
-   
-   s <- getState
-   let p = map (mkGeom (transform s) False (material s) (emit s)) sh
-   setState s {prims = map MkAnyPrim p ++ (prims s)}
-
-pMesh :: JobParser [Shape]
-pMesh = do
-   vc <- namedInt "vertexCount" <|> fail "vertexCount missing"
-   ws
-   fc <- namedInt "faceCount"  <|> fail "faceCount missing"
-   vertices <- count vc vertex
-   let va = V.fromList vertices
-   faces <- count fc (face va)
-   return (triangulate faces)
-   
-face :: (V.Vector Vertex) -> JobParser [Vertex]
-face vs = do
-   _ <- ws >> char 'f'
-   is <- many1 (try (do ws; integ))
-   return (map (vs V.!) is)
-
-vertex :: JobParser Vertex
-vertex = do
-   _ <- ws >> char 'v'
-   v <- ws >> pVec
-   return (Vertex v)
 
 --
 -- parsing light sources
