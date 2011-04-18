5682e0f3bec3b8bb2fd5d9544c4c2da84b994844
Matthias Treydte <waldheinz@gmail.com>
2011-04-18 18:35:50 +0200

    Added missing CameraParser.

---
+++ src/Graphics/Bling/IO/CameraParser.hs
@@ -0,0 +1,37 @@
+
+module Graphics.Bling.IO.CameraParser (
+   defaultCamera, pCamera
+   ) where
+
+import Text.ParserCombinators.Parsec
+
+import Graphics.Bling.Camera
+import Graphics.Bling.Math
+import Graphics.Bling.Transform
+import Graphics.Bling.IO.ParserCore
+
+defaultCamera :: Int -> Int -> Camera
+defaultCamera sx sy =
+   mkPerspectiveCamera c2w 0 1 90 (fromIntegral sx) (fromIntegral sy) where
+      c2w = translate (Vector 0 0 (-5))
+
+pCamera :: JobParser ()
+pCamera = (flip namedBlock) "camera" $ do
+   t <- many1 alphaNum
+   s <- getState
+   
+   cam <- case t of
+               "perspective" -> pPerspectiveCam s
+               _ -> fail $ "unknown camera type " ++ t
+   
+   setState s { camera = cam }
+
+pPerspectiveCam :: PState -> JobParser Camera
+pPerspectiveCam s = do
+   fov <- ws >> namedFloat "fov"
+   lr <- ws >> namedFloat "lensRadius"
+   fd <- ws >> namedFloat "focalDistance"
+   let sx = fromIntegral $ resX s
+   let sy = fromIntegral $ resY s
+   let c2w = transform s
+   return $ mkPerspectiveCamera c2w lr fd fov sx sy
