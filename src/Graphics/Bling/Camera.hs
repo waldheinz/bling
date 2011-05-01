0bab45c71bc5c59f6f707932fb4b610bab55a740
Matthias Treydte <waldheinz@gmail.com>
2011-05-01 22:53:19 +0200

    Fixed hlint warnings in Camera.

--- src/Graphics/Bling/Camera.hs
+++ src/Graphics/Bling/Camera.hs
@@ -46,7 +46,7 @@ fireRay (ProjectiveCamera c2w _ r2c _ _ lr fd) = do
          ro = mkPoint lu lv 0
          (lu, lv) = (\(u', v') -> (u'*lr, v'*lr)) (concentricSampleDisk luv)
          rd = normalize $ pFocus - ro
-         pFocus = rayAt ray (fd / (vz $ rayDir ray))
+         pFocus = rayAt ray (fd / vz (rayDir ray))
 
 mkProjective
    :: Transform -- ^ camera to world
@@ -67,7 +67,7 @@ mkProjective c2w p lr fd sx sy = ProjectiveCamera c2w p r2c s2r r2s lr fd where
    st2 = scale (Vector (1 / (s1 - s0)) (1 / (s2 - s3)) 1)
    t = translate (Vector (-s0) (-s3) 0)
    r2s = inverse s2r
-   r2c = r2s `concatTrans` (inverse p)
+   r2c = r2s `concatTrans` inverse p
    
 mkPerspectiveCamera
    :: Transform -- ^ the camera to world transform
