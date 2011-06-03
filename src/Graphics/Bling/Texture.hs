892abd004ddeadcd023a987742356ad006bef977
Matthias Treydte <waldheinz@gmail.com>
2011-06-03 12:38:13 +0200

    Added Perlin noise function.

--- src/Graphics/Bling/Texture.hs
+++ src/Graphics/Bling/Texture.hs
@@ -5,8 +5,11 @@ module Graphics.Bling.Texture (
    Texture, SpectrumTexture, ScalarTexture,
    
    -- * Creating Textures
-   constant, graphPaper, checkerBoard
+   constant, graphPaper, checkerBoard, noiseTexture
    ) where
+
+import Data.Bits
+import qualified Data.Vector.Unboxed as V
 
 import Graphics.Bling.Math
 import Graphics.Bling.Spectrum
@@ -42,3 +45,67 @@ checkerBoard (Vector sx sy sz) t1 t2 dg@(DifferentialGeometry (Vector x y z) _)
    | (floor (x * sx) + floor (y * sy) + floor (z * sz) :: Int) `mod` 2 == 0 = t1 dg
    | otherwise = t2 dg
    
+--
+-- Perlin Noise
+--
+
+noiseTexture :: ScalarTexture
+noiseTexture (DifferentialGeometry (Vector x y z) _) = perlin3d (x, y, z)
+
+perlin3d :: (Flt, Flt, Flt) -> Flt
+perlin3d (x, y, z) = lerp wz y0 y1 where
+   -- cell coordinates and offsets
+   (ix', iy', iz') = (floor x, floor y, floor z) :: (Int, Int, Int)
+   (dx, dy, dz) = (x - fromIntegral ix, y - fromIntegral iy, z - fromIntegral iz)
+   (ix, iy, iz) = (ix' .&. 255, iy' .&. 255, iz' .&. 255)
+   
+   -- gradient weights
+   w000 = grad  ix     iy     iz     dx     dy     dz
+   w100 = grad (ix+1)  iy     iz    (dx-1)  dy     dz
+   w010 = grad  ix    (iy+1)  iz     dx    (dy-1)  dz
+   w110 = grad (ix+1) (iy+1)  iz    (dx-1) (dy-1)  dz
+   w001 = grad  ix     iy    (iz+1)  dx     dy    (dz-1)
+   w101 = grad (ix+1)  iy    (iz+1) (dx-1)  dy    (dz-1)
+   w011 = grad  ix    (iy+1) (iz+1)  dx    (dy-1) (dz-1)
+   w111 = grad (ix+1) (iy+1) (iz+1) (dx-1) (dy-1) (dz-1)
+   
+   -- trilinear weight interpolation
+   y0 = lerp wy x00 x10
+   y1 = lerp wy x01 x11
+   x00 = lerp wx w000 w100
+   x10 = lerp wx w010 w110
+   x01 = lerp wx w001 w101
+   x11 = lerp wx w011 w111
+   wx = noiseWeight dx
+   wy = noiseWeight dy
+   wz = noiseWeight dz
+   
+noiseWeight :: Flt -> Flt
+noiseWeight t = 6 * t4 * t  - 15 * t4 + 10 * t3 where
+   t3 = t * t * t
+   t4 = t3 * t
+   
+grad :: Int -> Int -> Int -> Flt -> Flt -> Flt -> Flt
+grad x y z dx dy dz = u + v where
+   u = if (h .&. 1) /= 0 then (-u') else u'
+   v = if h .&. 2 /= 0 then (-v') else v'
+   u' = if h < 8 || h == 12 || h == 13 then dx else dy
+   v' = if h < 4 || h == 12 || h == 13 then dy else dz
+   h = h' .&. 15
+   h' = noisePerms V.! ((noisePerms V.! ((noisePerms V.! x) + y)) + z)
+   
+noisePerms :: V.Vector Int
+noisePerms = V.fromList (l ++ l) where
+  l = [ 151,160,137,91,90,15,
+   131,13,201,95,96,53,194,233,7,225,140,36,103,30,69,142,8,99,37,240,21,10,23,
+   190, 6,148,247,120,234,75,0,26,197,62,94,252,219,203,117,35,11,32,57,177,33,
+   88,237,149,56,87,174,20,125,136,171,168, 68,175,74,165,71,134,139,48,27,166,
+   77,146,158,231,83,111,229,122,60,211,133,230,220,105,92,41,55,46,245,40,244,
+   102,143,54, 65,25,63,161, 1,216,80,73,209,76,132,187,208, 89,18,169,200,196,
+   135,130,116,188,159,86,164,100,109,198,173,186, 3,64,52,217,226,250,124,123,
+   5,202,38,147,118,126,255,82,85,212,207,206,59,227,47,16,58,17,182,189,28,42,
+   223,183,170,213,119,248,152, 2,44,154,163, 70,221,153,101,155,167, 43,172,9,
+   129,22,39,253, 19,98,108,110,79,113,224,232,178,185, 112,104,218,246,97,228,
+   251,34,242,193,238,210,144,12,191,179,162,241, 81,51,145,235,249,14,239,107,
+   49,192,214, 31,181,199,106,157,184, 84,204,176,115,121,50,45,127, 4,150,254,
+   138,236,205,93,222,114,67,29,24,72,243,141,128,195,78,66,215,61,156,180 ]
