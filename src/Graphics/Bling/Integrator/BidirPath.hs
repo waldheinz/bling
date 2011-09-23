3b70c4e1f124678aa41754a3bd11fc045e781a64
Matthias Treydte <waldheinz@gmail.com>
2011-09-23 22:17:14 +0200

    Kind of finished bidir for non-specular scenes.

--- src/Graphics/Bling/Integrator/BidirPath.hs
+++ src/Graphics/Bling/Integrator/BidirPath.hs
@@ -4,7 +4,8 @@ module Graphics.Bling.Integrator.BidirPath (
    ) where
 
 import Data.BitSet
-import Control.Monad (liftM)
+import Debug.Trace
+import Control.Monad (liftM, forM)
 import qualified Text.PrettyPrint as PP
 
 import Graphics.Bling.Camera
@@ -18,29 +19,6 @@ import Graphics.Bling.Spectrum
 
 data BidirPath = BDP
 
-mkBidirPathIntegrator :: BidirPath
-mkBidirPathIntegrator = BDP
-
-instance Printable BidirPath where
-   prettyPrint (BDP) = PP.text "Bi-Dir Path"
-
-instance SurfaceIntegrator BidirPath where
-   sampleCount1D _ = 0
-   sampleCount2D _ = 0
-   
-   contrib (BDP) s addContrib' r = do
-      ep <- eyePath s r
-      lp <- lightPath s
-      
-   --   cws <- (mkContrib $ aws (contribS0 ep + contribS1 ep) (connect ep lp))
-      
-      contribS0 ep >>= addContrib
-      contribS1 s ep >>= addContrib
-      contribT1 s lp addContrib'
-      
-      where
-         addContrib = liftSampled . addContrib'
-         
 -- | a path vertex
 data Vertex = Vert
    { _vbsdf    :: Bsdf
@@ -54,6 +32,92 @@ data Vertex = Vert
    }
 
 type Path = [Vertex]
+
+mkBidirPathIntegrator :: BidirPath
+mkBidirPathIntegrator = BDP
+
+instance Printable BidirPath where
+   prettyPrint (BDP) = PP.text "Bi-Dir Path"
+
+instance SurfaceIntegrator BidirPath where
+   sampleCount1D _ = 0
+   sampleCount2D _ = 0
+   
+   contrib (BDP) scene addContrib' r = do
+      ep <- eyePath scene r
+      lp <- lightPath scene
+      
+      -- direct illumination, aka "one light" or S1 subpaths
+      ld <- liftM sum $ forM (zip ep [0..]) $ \(v, i) -> do
+         d <- estimateDirect scene v
+         return $ sScale d $ 1 / (fromIntegral i + 1) -- - nSpecularVertices[i+1]
+
+      let prevSpec = True : map (\v -> Specular `member` _vtype v) ep
+
+      -- light sources directly visible, or by specular reflection
+      let le = sum $ map (\v -> _vle v * _valpha v) $ map fst $ filter snd $ zip ep prevSpec
+         
+      
+      let ei = zip ep [0..]
+      let li = zip lp [0..]
+      
+      let l = sum $ map (connect scene) $ pairs ei li
+      mkContrib (1, l + ld + le) False >>= addContrib
+      where
+         addContrib = liftSampled . addContrib'
+
+--------------------------------------------------------------------------------
+-- Path Evaluation
+--------------------------------------------------------------------------------
+
+connect :: Scene -> ((Vertex, Int),  (Vertex, Int)) -> Spectrum
+connect scene
+   ((Vert bsdfe pe ne wie woe le te alphae, i),  -- eye vertex
+    (Vert bsdfl pl nl wil wol ll tl alphal, j))   -- camera vertex
+       | Specular `member` te = black
+       | Specular `member` tl = black
+       | isBlack fe || isBlack fl = black
+       | scene `intersects` r = black
+       | otherwise = sScale (alphae * fe * alphal * fl) (g * pathWt)
+       where
+          pathWt = 1 / fromIntegral (i + j + 2) -- - nSpecularVertices[i+j+2]);
+          g = absDot ne w * absDot nl w / sqLen (pl - pe)
+          w = normalize $ pl - pe
+          fe = evalBsdf bsdfe woe  w -- * (1 + vc.nSpecularComponents);
+          fl = evalBsdf bsdfl (-w) wol -- * (1 + vc.nSpecularComponents);
+          r = segmentRay pl pe
+          
+estimateDirect :: Scene -> Vertex -> Sampled s Spectrum
+estimateDirect scene (Vert bsdf p n wi wo l t alpha) = do
+   lNumU <- rnd
+   lDirU <- rnd2D
+   lBsdfCompU <- rnd
+   lBsdfDirU <- rnd2D
+   let lHere = sampleOneLight scene p n wo bsdf $ RLS lNumU lDirU lBsdfCompU lBsdfDirU
+   return $ lHere * alpha
+
+pairs :: [a] -> [a] -> [(a, a)]
+pairs [] _ = []
+pairs _ [] = []
+pairs (x:xs) ys = zip (repeat x) ys ++ pairs xs ys
+
+-- | follow specular paths from the light and connect the to the eye
+contribT1 :: Scene -> Path -> Consumer m -> Sampled m ()
+contribT1 _ [] _ = return ()
+contribT1 sc ((Vert bsdf p n _ wi _ t le):vs) tell
+   | Specular `member` t = contribT1 sc vs tell
+   | intersects sc ray = contribT1 sc vs tell
+   | otherwise = liftSampled $ tell (True, smp)
+   where
+      f = csf * le * evalBsdf bsdf wi we
+      g = absDot n wi * absDot n we / (cPdf * d2)
+      smp = ImageSample px py (1 /g, f)
+      ray = segmentRay p pCam
+--      let smpHere = ImageSample px py (absDot n we / (cPdf * dCam2), f * li * csf)
+      (CameraSampleResult csf pCam px py cPdf) = sampleCam (sceneCam sc) p
+      dCam = pCam - p
+      we = normalize $ dCam
+      d2 = sqLen dCam -- ^ distance squared
 
 --------------------------------------------------------------------------------
 -- Path Generation
@@ -110,54 +174,3 @@ nextVertex sc wi (Just int) alpha depth = do
 
       n = normalize $ dgN dg
       p = dgP dg
-
---------------------------------------------------------------------------------
--- Path Evaluation
---------------------------------------------------------------------------------
-   
--- | contribution for the eye subpath randomly intersecting a light
---   source, or for light sources visible through specular reflections
-contribS0 :: Path -> Sampled s Contribution
-contribS0 p = mkContrib (1, s0 p) False where
-   s0 [] = black
-   s0 ((Vert _ _ _ _ _ l t f) : vs)
-      | Specular `member` t = f * l + s0 vs
-      | otherwise = f * l
-
--- | one light subpaths
-contribS1 :: Scene -> Path -> Sampled s Contribution
-contribS1 scene path = s1 path >>= \x -> mkContrib (1, x) False where
-   s1 [] = return black
-   s1 ((Vert bsdf p n _ wo _ t f) : vs)
-      | Specular `member` t = s1 vs
-      | otherwise = do
-
-         -- for light sampling
-         lNumU <- rnd
-         lDirU <- rnd2D
-         lBsdfCompU <- rnd
-         lBsdfDirU <- rnd2D
-         let lHere = sampleOneLight scene p n wo bsdf $ RLS lNumU lDirU lBsdfCompU lBsdfDirU
-         return $ lHere * f
-   
--- | follow specular paths from the light and connect the to the eye
-contribT1 :: Scene -> Path -> Consumer m -> Sampled m ()
-contribT1 _ [] _ = return ()
-contribT1 sc ((Vert bsdf p n _ wi _ t le):vs) tell
-   | Specular `member` t = contribT1 sc vs tell
-   | intersects sc ray = contribT1 sc vs tell
-   | otherwise = liftSampled $ tell (True, smp)
-   where
-      f = csf * le * evalBsdf bsdf wi we
-      g = absDot n wi * absDot n we / (cPdf * d2)
-      smp = ImageSample px py (1 /g, f)
-      ray = segmentRay p pCam
---      let smpHere = ImageSample px py (absDot n we / (cPdf * dCam2), f * li * csf)
-      (CameraSampleResult csf pCam px py cPdf) = sampleCam (sceneCam sc) p
-      dCam = pCam - p
-      we = normalize $ dCam
-      d2 = sqLen dCam -- ^ distance squared
-{-
-connect :: Path -> Path -> WeightedSpectrum
-connect ep lp = (1, black)
--}
